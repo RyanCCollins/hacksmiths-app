932448cf6f3b7712badfb807fc15f1f6b403a20b
Ryan Collins <rccproductions@mac.com>
2016-03-22 14:03:02 -0400

    Feat: add leaderboard controller

---
+++ hacksmiths/LeaderboardTableViewController.swift
@@ -0,0 +1,88 @@
+//
+//  LeaderboardTableViewController.swift
+//  hacksmiths
+//
+//  Created by Ryan Collins on 3/22/16.
+//  Copyright © 2016 Tech Rapport. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class LeaderboardTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("LeaderBoardTableViewCell", forIndexPath: indexPath) as! LeaderBoardTableViewCell
+        
+
+        
+        return cell
+    }
+ 
+    
+    lazy var leaderFetchResultsController: NSFetchedResultsController = {
+        let sortPriority = NSSortDescriptor(key: "sortPriority", ascending: true)
+        
+        let isPublicButNotLeaderPredicate = NSPredicate(format: "isPublic == %@ && isTopContributor == %@", NSNumber(bool: true), NSNumber(bool: false))
+        let isPublicButNotLeaderFetch = NSFetchRequest(entityName: "Person")
+        isPublicButNotLeaderFetch.sortDescriptors = [sortPriority]
+        isPublicButNotLeaderFetch.predicate = isPublicButNotLeaderPredicate
+        
+        let fetchResultsController = NSFetchedResultsController(fetchRequest: isPublicButNotLeaderFetch, managedObjectContext: self.sharedContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        do {
+            try fetchResultsController.performFetch()
+        } catch let error {
+            print(error)
+        }
+        
+        return fetchResultsController
+    }()
+    
+
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+
+        return false
+    }
+
+
+    /*
+     MARK: - Navigation
+
+     In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+         Get the new view controller using segue.destinationViewController.
+         Pass the selected object to the new view controller.
+    }
+    */
+
+}
