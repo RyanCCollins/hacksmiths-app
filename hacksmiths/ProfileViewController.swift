12c9d91c9c14a958b4e80a4b0b773417a03eaf40
Ryan Collins <rccproductions@mac.com>
2016-03-24 13:33:15 -0400

    Feat: add image editing capability to profile

--- hacksmiths/ProfileViewController.swift
+++ hacksmiths/ProfileViewController.swift
@@ -8,20 +8,23 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate {
     
     
+    @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var nameLabel: UILabel!
     
     @IBOutlet weak var editButton: UIBarButtonItem!
 
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var descriptionLabel: UILabel!
+
+    @IBOutlet weak var tapToEditPhotoLabel: UILabel!
     @IBOutlet weak var twitterLabel: UILabel!
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var profileTextView: UITextView!
     @IBOutlet weak var descriptionTextField: UITextView!
 
+    @IBOutlet weak var profilePhotoViewOverlay: UIView!
     @IBOutlet weak var cancelButton: UIBarButtonItem!
     @IBOutlet weak var noDataFoundLabel: UILabel!
     
@@ -31,11 +34,33 @@ class ProfileViewController: UIViewController {
 
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func addTouchRecognizer() {
+        let fingerTouchGestureRecognizer = UIGestureRecognizer(target: profilePhotoViewOverlay, action: "didTapEditPhoto:")
+        profilePhotoViewOverlay.addGestureRecognizer(fingerTouchGestureRecognizer)
     }
+    
+    func hideUIElements() {
+        profilePhotoViewOverlay.hidden = true
+        cameraButton.hidden = true
+        tapToEditPhotoLabel.hidden = true
+    }
+    
+    func didTapEditPhoto(sender: UIGestureRecognizer) {
+        if sender.numberOfTouches() == 1 && editing == true {
+            editProfileImagePhoto()
+        }
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // We check that the user is authenticated and enable the edit button
+        // Based on their status.
+        editButton.enabled = UserData.sharedInstance().authenticated
+
+    }
+    
     @IBAction func didTapEditButtonUpInside(sender: AnyObject) {
         // Switch the editing toggle
         editing = !editing
@@ -59,16 +84,17 @@ class ProfileViewController: UIViewController {
         cancelButton.enabled = editing
         nameLabel.hidden = editing
         nameTextField.hidden = !editing
-        descriptionLabel.hidden = editing
+
         descriptionTextField.hidden = !editing
-        twitterLabel.hidden = editing
-        githubLabel.hidden = editing
         
     }
     
-    func updateProfile() {
+    func editProfileImagePhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
         
     }
+    
     
 
 }
