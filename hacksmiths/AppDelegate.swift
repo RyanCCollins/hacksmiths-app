d747a91bcbd461db42c5635aa67d3357106cd6e6
Ryan Collins <rccproductions@mac.com>
2016-06-02 19:37:02 -0400

    Feat: add crashlytics to project

--- hacksmiths/AppDelegate.swift
+++ hacksmiths/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Fabric.with([Crashlytics.self])
         registerForPushNotifications(application)
         return true
     }
